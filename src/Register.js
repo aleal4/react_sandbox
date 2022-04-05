0505757cdbec6775173f54e76bc9cd195e1f6c20
Augusto Leal <aleal4@gmail.com>
2022-04-04 21:54:49 -0700

    axios implementation

--- src/Register.js
+++ src/Register.js
@@ -1,27 +1,52 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Register = () => {
   const [firstName, setFirstName] = useState('');
   const [password, setPassword] = useState('');
+  const [allUsers, setAllUsers] = useState([]);
 
-  console.log(firstName, password);
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newUser = { firstName: firstName, password: password };
+    setAllUsers({ ...allUsers, newUser });
+  };
+
+  const formCleanup = () => {
+    setPassword('');
+    setFirstName('');
+  };
 
   return (
     <div>
       Rigester
-      <form>
+      <form onSubmit={handleSubmit}>
         first name:
         <input
           type="text"
           name="firstName"
+          value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
         />
         Password:
         <input
           type="password"
           name="password"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <button>Submit</button>
       </form>
     </div>
   );
